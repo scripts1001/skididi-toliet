87b994180031b96a1c738f7215c5991ef2a45724
scripts1001 <93733356+scripts1001@users.noreply.github.com>
2024-04-13 18:16:09 -0400

    Update dopdop.lua

--- dopdop.lua
+++ dopdop.lua
@@ -12,7 +12,7 @@ local notificationPositions = {
     ["MiddleRight"] = UDim2.new(0.85, 0, 0.7, 0),
     ["MiddleLeft"] = UDim2.new(0.01, 0, 0.7, 0),
     
-    ["Top"] = UDim2.new(0.445, 0, 0.007, 0),
+    ["Top"] = UDim2.new(0.445, 0, 0.002, 0),
     ["TopLeft"] = UDim2.new(0.06, 0, 0.001, 0),
     ["TopRight"] = UDim2.new(0.8, 0, 0.001, 0),
 }; 
