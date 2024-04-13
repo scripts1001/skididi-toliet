b51df46233920a7e9031b479e419c9ee97426bcb
scripts1001 <93733356+scripts1001@users.noreply.github.com>
2024-04-13 17:38:49 -0400

    Update dopdopdopp.lua

--- dopdopdopp.lua
+++ dopdopdopp.lua
@@ -1,53 +1,39 @@
 local library = {}
-
 local TweenService = game:GetService("TweenService")
 function library:tween(...) TweenService:Create(...):Play() end
-
 local uis = game:GetService("UserInputService")
-
 function library:create(Object, Properties, Parent)
     local Obj = Instance.new(Object)
-
     for i,v in pairs (Properties) do
         Obj[i] = v
     end
     if Parent ~= nil then
         Obj.Parent = Parent
     end
-
     return Obj
 end
-
 local text_service = game:GetService("TextService")
 function library:get_text_size(...)
     return text_service:GetTextSize(...)
 end
-
 function library:console(func)
     func(("\n"):rep(57))
 end
-
 library.signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Signal.lua"))()
-
 local local_player = game:GetService("Players").LocalPlayer
 local mouse = local_player:GetMouse()
-
 local http = game:GetService("HttpService")
 local rs = game:GetService("RunService")
-
 function library:set_draggable(gui)
     local UserInputService = game:GetService("UserInputService")
-
     local dragging
     local dragInput
     local dragStart
     local startPos
-
     local function update(input)
         local delta = input.Position - dragStart
         gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
     end
-
     gui.InputBegan:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
             dragging = true
@@ -61,25 +47,21 @@ function library:set_draggable(gui)
             end)
         end
     end)
-
     gui.InputChanged:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
             dragInput = input
         end
     end)
-
     UserInputService.InputChanged:Connect(function(input)
         if input == dragInput and dragging then
             update(input)
         end
     end)
 end
-
 function library.new(library_title, cfg_location)
     local menu = {}
     menu.values = {}
     menu.on_load_cfg = library.signal.new("on_load_cfg")
-
     if not isfolder(cfg_location) then
         makefolder(cfg_location)
     end
@@ -101,18 +83,15 @@ function library.new(library_title, cfg_location)
                 for _,sector in next, section do
                     for _,element in next, sector do
                         if not element.Color then continue end
-
                         element.Color = {R = element.Color.R, G = element.Color.G, B = element.Color.B}
                     end
                 end
             end
         end
-
         writefile(cfg_location..cfg_name..".txt", http:JSONEncode(values_copy))
     end
     function menu.load_cfg(cfg_name)
         local new_values = http:JSONDecode(readfile(cfg_location..cfg_name..".txt"))
-
         for _,tab in next, new_values do
             for _2,section in next, tab do
                 for _3,sector in next, section do
@@ -120,7 +99,6 @@ function library.new(library_title, cfg_location)
                         if element.Color then
                             element.Color = Color3.new(element.Color.R, element.Color.G, element.Color.B)
                         end
-
                         pcall(function()
                             menu.values[_][_2][_3][_4] = element
                         end)
@@ -128,10 +106,8 @@ function library.new(library_title, cfg_location)
                 end
             end
         end
-
         menu.on_load_cfg:Fire()
     end
-
     menu.open = true
     local ScreenGui = library:create("ScreenGui", {
         ResetOnSpawn = false,
@@ -139,11 +115,9 @@ function library.new(library_title, cfg_location)
         Name = "unknown",
         IgnoreGuiInset = true,
     })
-
 	if syn then
 		syn.protect_gui(ScreenGui)
 	end
-
     local Cursor = library:create("ImageLabel", {
         Name = "Cursor",
         BackgroundTransparency = 1,
@@ -151,32 +125,25 @@ function library.new(library_title, cfg_location)
         Image = "rbxassetid://7205257578",
         ZIndex = 6969,
     }, ScreenGui)
-
     rs.RenderStepped:Connect(function()
         Cursor.Position = UDim2.new(0, mouse.X, 0, mouse.Y + 36)
     end)
-
 	ScreenGui.Parent = game:GetService("CoreGui")
-
     function menu.IsOpen()
         return menu.open
     end
     function menu.SetOpen(State)
         ScreenGui.Enabled = state
     end
-
     uis.InputBegan:Connect(function(key)
         if key.KeyCode ~= Enum.KeyCode.Insert then return end
-
 		ScreenGui.Enabled = not ScreenGui.Enabled
         menu.open = ScreenGui.Enabled
-
         while ScreenGui.Enabled do
             uis.MouseIconEnabled = true
             rs.RenderStepped:Wait()
         end
 	end)
-
     local ImageLabel = library:create("ImageButton", {
         Name = "Main",
         AnchorPoint = Vector2.new(0.5, 0.5),
