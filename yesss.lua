f6fb02f69952be0a4c96351b5efe4fe5f40faa7b
scripts1001 <93733356+scripts1001@users.noreply.github.com>
2024-04-13 18:44:15 -0400

    Update yesss.lua

--- yesss.lua
+++ yesss.lua
@@ -1,10 +1,18 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/scripts1001/skididi-toliet/main/yess.lua"))()
-local Notify = notif.Notify;
-Notify({
-Description = "Emerald, Loaded.";
-Title = "Enjoy!";
-Duration = 3;
+local notificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/scripts1001/skididi-toliet/main/dopdop.lua"))();
+local notifications = notificationLibrary.new({            
+    NotificationLifetime = 3, 
+    NotificationPosition = "Top",
+    
+    TextFont = Enum.Font.Code,
+    TextColor = Color3.fromRGB(0, 0, 0),
+    TextSize = 15,
+    
+    TextStrokeTransparency = 0, 
+    TextStrokeColor = Color3.fromRGB(80, 200, 120)
 });
+
+notifications:BuildNotificationUI();
+notifications:Notify("Emerald Silent Aim, Loaded.");
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/scripts1001/skididi-toliet/main/yess.lua"))()
 
